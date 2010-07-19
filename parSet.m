e4b283ac52aceb16fe864b4d8c9b4bda15ee91fd
unknown <julian@.(none)>
2010-07-19 02:41:54 -0400

    included par.wage and par.max_edu

--- parSet.m
+++ parSet.m
@@ -1,10 +1,11 @@
-function par = parSet(rho,lambdam,lambdah,r,w,muh,mum,theta,sigma,a,edu)
+function par = parSet(rho,lambdam,lambdah,r,wage,max_edu,muh,mum,theta,sigma,a,edu)
     par.rho=rho;
     par.beta=1/(1+rho);
     par.lambdam=lambdam;
     par.lambdah=lambdah;
     par.r=r;
-    par.w=w;
+    par.wage=wage;
+    par.max_edu=max_edu;
     par.muh=muh;
     par.mum=mum;
     par.theta=theta;
