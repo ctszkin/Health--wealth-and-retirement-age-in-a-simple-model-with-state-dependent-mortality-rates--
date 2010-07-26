5a5e40759c4c78eaeaf6c99bf03fc14c1785d3f9
Tsz Kin Chan <ctszkin@gmail.com>
2010-07-26 02:13:32 -0400

    updated the agentDecision.m, the new code is more readable now.

--- agentDecision.m
+++ agentDecision.m
@@ -1,100 +1,112 @@
-function result = agentDecision(state,par,critical_point)      
+  function result = agentDecision(state,model)
+        
+        HEALTHY=0;
+        MORBID=1;
+        DEAD=2;
+        STUDYING=0;
+        WORKING=1;
+        RETIRED=2;
+        
+
+
+%         HEALTHY='Healthy';
+%         MORBID='Morbid';
+%         DEAD='Dead';
+%         STUDYING='Studying';
+%         WORKING='Working';
+%         RETIRED='Retired';
+%         
+
+
+
         lifeSpan=sum(state);
-%       result: age health status e a c
-        result=nan(lifeSpan+1,6);
-        result(:,1)=1:(lifeSpan+1);
-        result(1: state(1),2)=0;
-        result(state(1)+1: state(2)+state(1),2)=1;
-        result(lifeSpan+1,2)=2;
+
+        result.age = (1:(lifeSpan+1))';
         
-        [temp pos_of_a_zero]=max(par.a==0)
+        result.health = nan(lifeSpan+1,1);
+        result.health(1: state(1))=HEALTHY;
+        result.health(state(1)+1: state(2)+state(1))=MORBID;
+        result.health(lifeSpan+1)=DEAD;
         
-        result(1,5)=pos_of_a_zero;
-        result(1,4)=0;
+       result.employmentStatus= nan(lifeSpan+1,1);
+       result.yearOfEducation= nan(lifeSpan+1,1);
+       result.asset = nan(lifeSpan+1,1);
+       result.status= nan(lifeSpan+1,1);
+       
+        
+        [temp postitionOfZeroInAsset]=max(model.par.a==0);
+
         
         % period 1
-        if result(1,2)==0  % if healthy
-%             if result(1,5) < critical_point_healthy_schooling(1)
-            if result(1,5) < critical_point(3,1)
-                result(2,5) = h_hs(result(1,5),1);
-                result(1,3) = 0;
-                result(2,4) = 1;
-%             elseif result(1,5) >= critical_point_healthy_retirement(1)
-            elseif result(1,5) >= critical_point(4,1)
-                result(2,5) = h_hr(result(1,5));
-                result(1,3) = 2;
-                result(2,4) = 0;
+        result.asset(1)=postitionOfZeroInAsset;
+        result.yearOfEducation(1)=0;
+
+        if result.health(1)==HEALTHY  
+            if result.asset(1) <  model.criticalPoint.healthy_schooling2working(1)
+                result.status(1) = STUDYING;
+                result.asset(2) = model.policyFn.healthy_schooling(result.asset(1),1);
+                result.yearOfEducation(2) = 1;
+            elseif result.asset(1) >= model.criticalPoint.healthy_working2retired(1)
+                result.status(1) = RETIRED;
+                result.asset(2) =  model.policyFn.healthy_retired(result.asset(1));
+                result.yearOfEducation(2) = 0;
             else
-                result(2,5) = h_hw(result(1,5),1);
-                result(1,3) = 1;
-                result(2,4) = 0;
+                result.status(1) = WORKING;
+                result.asset(2) = model.policyFn.healthy_working(result.asset(1),1);
+                result.yearOfEducation(2) = 0;
             end 
         end
-        if result(1,2)==1  % if morbid
-%             if result(1,5) < critical_point_morbid_schooling(1)
-            if result(1,5) < critical_point(1,1)
-                result(2,5) = h_ms(result(1,5),1);
-                result(1,3) = 0;
-                result(2,4) = 1;
-%             elseif result(1,5) >= critical_point_morbid_retirement(1)
-             elseif result(1,5) >= critical_point(2,1)
-                result(2,5) = h_mr(result(1,5));
-                result(1,3) = 2;
-                result(2,4) = 0;
+        if result.health(1)==MORBID 
+            if result.asset(1) < model.criticalPoint.morbid_schooling2working(1)
+                result.status(1) = STUDYING;
+                result.asset(2) = model.policyFn.morbid_schooling(result.asset(1),1);
+                result.yearOfEducation(2) = 1;
+             elseif result.asset(1) >= model.criticalPoint.morbid_working2retired(1)
+                result.status(1) = RETIRED;
+                 result.asset(2) =  model.policyFn.morbid_retired(result.asset(1));
+                result.yearOfEducation(2) = 0;
             else
-                result(2,5) = h_mw(result(1,5),1);
-                result(1,3) = 1;
-                result(2,4) = 0;
+                result.status(1) = WORKING;
+                result.asset(2) = model.policyFn.morbid_working(result.asset(1),1);
+                result.yearOfEducation(2) = 0;
             end 
         end
         
         
         % period 1+
-        for j=2:size(result,1)            
-            if result(j,2)==0  % if healthy
-%                 if result(j,5) <
-%                 critical_point_healthy_schooling(result(j,4)+1)
-                if result(j,5) < critical_point(3,result(j,4)+1)
-                    result(j+1,5) = h_hs(result(j,5),result(j,4)+1);
-                    result(j,3) = 0;
-                    if result(j,4) < par.max_edu
-                        result(j+1,4) = result(j,4)+1;
-                    else
-                         result(j+1,4) = result(j,4);
-                    end
-%                 elseif result(j,5) >=
-%                 critical_point_healthy_retirement(result(j,4)+1)
-                elseif result(j,5) >= critical_point(4,result(j,4)+1) 
-                    result(j+1,5) = h_hr(result(j,5));
-                    result(j,3) = 2;
-                    result(j+1,4) = result(j,4);
+        for j=2:lifeSpan            
+            if result.health(j)==HEALTHY  
+                if result.asset(j) < model.criticalPoint.healthy_schooling2working(result.yearOfEducation(j)+1)
+                    result.status(j) = STUDYING;
+                    result.asset(j+1) = model.policyFn.healthy_schooling(result.asset(j),result.yearOfEducation(j)+1);
+                    result.yearOfEducation(j+1) = result.yearOfEducation(j)+1;
+                elseif result.asset(j) >= model.criticalPoint.healthy_working2retired(result.yearOfEducation(j)+1) 
+                    result.status(j) = RETIRED;
+                    result.asset(j+1) = model.policyFn.healthy_retired(result.asset(j));
+                    result.yearOfEducation(j+1) = result.yearOfEducation(j);
                 else
-                    result(j+1,5) = h_hw(result(j,5),result(j,4)+1);
-                    result(j,3) = 1;
-                    result(j+1,4) = result(j,4);
+                    result.status(j) = WORKING;
+                    result.asset(j+1) = model.policyFn.healthy_working(result.asset(j),result.yearOfEducation(j)+1);
+                    result.yearOfEducation(j+1) = result.yearOfEducation(j);
                 end 
             end
-            if result(j,2)==1  % if morbid
-%                 if result(j,5) < critical_point_morbid_schooling(result(j,4)+1)
-                 if result(j,5) < critical_point(1,result(j,4)+1)
-                    result(j+1,5) = h_ms(result(j,5),result(j,4)+1);
-                    result(j,3) = 0;
-                    if result(j,4) < par.max_edu
-                        result(j+1,4) = result(j,4)+1;
-                    else
-                         result(j+1,4) = result(j,4);
-                    end
-%                 elseif result(1,5) >=
-%                 critical_point_morbid_retirement(result(j,4)+1)
-                elseif result(1,5) >= critical_point(2,result(j,4)+1)
-                    result(j+1,5) = h_mr(result(j,5));
-                    result(j,3) = 2;
-                    result(j+1,4) = result(j,4);
+            if result.health(j)==MORBID 
+                if result.asset(j) < model.criticalPoint.morbid_schooling2working(result.yearOfEducation(j)+1)
+                    result.status(j) = STUDYING;
+                    result.asset(j+1) = model.policyFn.morbid_schooling(result.asset(j),result.yearOfEducation(j)+1);
+                    result.yearOfEducation(j+1) = result.yearOfEducation(j)+1;
+                elseif result.asset(j) >= model.criticalPoint.morbid_working2retired(result.yearOfEducation(j)+1)
+                    result.status(j) = RETIRED;
+                    result.asset(j+1) = model.policyFn.morbid_retired(result.asset(j));
+                    result.yearOfEducation(j+1) = result.yearOfEducation(j);
                 else
-                    result(j+1,5) = h_mw(result(j,5),result(j,4)+1);
-                    result(j,3) = 1;
-                    result(j+1,4) = result(j,4);
+                    result.status(j) = WORKING;
+                    result.asset(j+1) = model.policyFn.morbid_working(result.asset(j));
+                    result.yearOfEducation(j+1) = result.yearOfEducation(j);
                 end 
             end
         end
-    end
+ end
+    
+
+
