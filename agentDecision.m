9035c7a81b5e8ecd79f72ab4247aa4e74473b087
Tsz Kin Chan <ctszkin@gmail.com>
2010-07-26 00:23:49 -0400

    bug fixed: There's no +1 in (f result(j,4)+1 < par.max_edu)

--- agentDecision.m
+++ agentDecision.m
@@ -1,12 +1,11 @@
-function result = agentDecision(state)      
+function result = agentDecision(state,par,critical_point)      
         lifeSpan=sum(state);
 %       result: age health status e a c
-        result=nan(lifeSpan,6)
-        result(:,1)=1:lifeSpan
-        result(1: state(1),2)=0
-        result(state(1)+1: state(2)+state(1),2)=1
-
-
+        result=nan(lifeSpan+1,6);
+        result(:,1)=1:(lifeSpan+1);
+        result(1: state(1),2)=0;
+        result(state(1)+1: state(2)+state(1),2)=1;
+        result(lifeSpan+1,2)=2;
         
         [temp pos_of_a_zero]=max(par.a==0)
         
@@ -15,11 +14,13 @@ function result = agentDecision(state)
         
         % period 1
         if result(1,2)==0  % if healthy
-            if result(1,5) > critical_point_healthy_schooling(1)
+%             if result(1,5) < critical_point_healthy_schooling(1)
+            if result(1,5) < critical_point(3,1)
                 result(2,5) = h_hs(result(1,5),1);
                 result(1,3) = 0;
                 result(2,4) = 1;
-            elseif result(1,5) >= critical_point_healthy_retirement(1)
+%             elseif result(1,5) >= critical_point_healthy_retirement(1)
+            elseif result(1,5) >= critical_point(4,1)
                 result(2,5) = h_hr(result(1,5));
                 result(1,3) = 2;
                 result(2,4) = 0;
@@ -30,11 +31,13 @@ function result = agentDecision(state)
             end 
         end
         if result(1,2)==1  % if morbid
-            if result(1,5) > critical_point_morbid_schooling(1)
+%             if result(1,5) < critical_point_morbid_schooling(1)
+            if result(1,5) < critical_point(1,1)
                 result(2,5) = h_ms(result(1,5),1);
                 result(1,3) = 0;
                 result(2,4) = 1;
-            elseif result(1,5) >= critical_point_morbid_retirement(1)
+%             elseif result(1,5) >= critical_point_morbid_retirement(1)
+             elseif result(1,5) >= critical_point(2,1)
                 result(2,5) = h_mr(result(1,5));
                 result(1,3) = 2;
                 result(2,4) = 0;
@@ -49,15 +52,19 @@ function result = agentDecision(state)
         % period 1+
         for j=2:size(result,1)            
             if result(j,2)==0  % if healthy
-                if result(j,5) > critical_point_healthy_schooling(result(j,4)+1)
+%                 if result(j,5) <
+%                 critical_point_healthy_schooling(result(j,4)+1)
+                if result(j,5) < critical_point(3,result(j,4)+1)
                     result(j+1,5) = h_hs(result(j,5),result(j,4)+1);
                     result(j,3) = 0;
-                    if result(j,4)+1 < par.max_edu
+                    if result(j,4) < par.max_edu
                         result(j+1,4) = result(j,4)+1;
                     else
                          result(j+1,4) = result(j,4);
                     end
-                elseif result(j,5) >= critical_point_healthy_retirement(result(j,4)+1)
+%                 elseif result(j,5) >=
+%                 critical_point_healthy_retirement(result(j,4)+1)
+                elseif result(j,5) >= critical_point(4,result(j,4)+1) 
                     result(j+1,5) = h_hr(result(j,5));
                     result(j,3) = 2;
                     result(j+1,4) = result(j,4);
@@ -68,15 +75,18 @@ function result = agentDecision(state)
                 end 
             end
             if result(j,2)==1  % if morbid
-                if result(j,5) > critical_point_morbid_schooling(result(j,4)+1)
+%                 if result(j,5) < critical_point_morbid_schooling(result(j,4)+1)
+                 if result(j,5) < critical_point(1,result(j,4)+1)
                     result(j+1,5) = h_ms(result(j,5),result(j,4)+1);
                     result(j,3) = 0;
-                    if result(j,4)+1 < par.max_edu
+                    if result(j,4) < par.max_edu
                         result(j+1,4) = result(j,4)+1;
                     else
                          result(j+1,4) = result(j,4);
                     end
-                elseif result(1,5) >= critical_point_morbid_retirement(result(j,4)+1)
+%                 elseif result(1,5) >=
+%                 critical_point_morbid_retirement(result(j,4)+1)
+                elseif result(1,5) >= critical_point(2,result(j,4)+1)
                     result(j+1,5) = h_mr(result(j,5));
                     result(j,3) = 2;
                     result(j+1,4) = result(j,4);
@@ -87,6 +97,4 @@ function result = agentDecision(state)
                 end 
             end
         end
-        
-        
     end
